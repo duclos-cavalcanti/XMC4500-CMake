382fab55bedad6c7f4b45eb4400b8e114fbef298
Daniel <daniel.duclos.cavalcanti@gmail.com>
2021-05-13 21:42:57 +0200

    Succesfully ported Uni Makefile project to CMake!

--- cmake/xmc4500.cmake
+++ cmake/xmc4500.cmake
@@ -1,3 +1,7 @@
+# XMC Model Specific
+set (XMC_SERIES 4500)
+set (XMC_PACKAGE F100)
+set (XMC_SIZE 1024)
 # Set cross compilation information
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
@@ -7,7 +11,13 @@ set(TOOLCHAIN_PREFIX "arm-none-eabi")
 
 set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}-gcc)
 set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}-g++)
-set(CMAKE_ASM_COMPILER ${TOOLCHAIN_PREFIX}-as)
+
+# Need to set to gcc and not as due to options in ASM_FLAGS
+# that apparently are not readable by the assembler directly. 
+# Have done so as this is a port of a makefile toolchain to
+# cmake, so there are grey areas in my understanding.
+set(CMAKE_ASM_COMPILER ${TOOLCHAIN_PREFIX}-gcc)
+
 set(CMAKE_AR ${TOOLCHAIN_PREFIX}-ar)
 set(CMAKE_OBJCOPY ${TOOLCHAIN_PREFIX}-objcopy)
 set(CMAKE_OBJDUMP ${TOOLCHAIN_PREFIX}-objdump)
@@ -15,20 +25,19 @@ set(CMAKE_OBJDUMP ${TOOLCHAIN_PREFIX}-objdump)
 enable_language(ASM)
 set(XMC -DXMC${XMC_SERIES}_${XMC_PACKAGE}x${XMC_SIZE})
 set(CPU "-mcpu=cortex-m4")
-set(FPU "-mfpu=fpv4-sp-d16 -mfloat-abi=softfp")
+set(FPU "-mfloat-abi=softfp -mfpu=fpv4-sp-d16")
+set(DBG "-g3 -gdwarf-2 -Wall")
+set(OPT "-O0 -ffunction-sections")
 
 set(CMAKE_ASM_FLAGS 
-    "${CMAKE_ASM_FLAGS} ${XMC} -mthumb ${CPU} ${FPU} -g3 -gdwarf-2 -c Wa,-adhlns='$@.lst' -Wall -x assembler-with-cpp"
+    "${CMAKE_ASM_FLAGS} ${XMC} ${CPU} ${FPU} -mthumb ${DBG}"
     )
+
 set(CMAKE_C_FLAGS 
-    "${CMAKE_C_FLAGS} ${XMC} -mthumb ${CPU} ${FPU} -std=gnu99 -Os -g3 -gdwarf-2 -c -Wa -adhlns='$@.lst' -Wall -ffunction-sections"
+    "${CMAKE_C_FLAGS} ${XMC} ${CPU} ${FPU} -mthumb ${DBG} -std=gnu99 ${OPT}"
     )
 
-set(CMAKE_SHARED_LIBRARY_LINK_C_FLAGS "-specs=nano.specs -specs=nosys.specs")
+set(CMAKE_SHARED_LIBRARY_LINK_C_FLAGS "-specs=nosys.specs")
 set(CMAKE_EXE_LINKER_FLAGS 
-    "-T${PROJECT_SOURCE_DIR}/build/main.ld -nostartfiles -L${XMC_LIB_DIR}/CMSIS/Lib/GCC/ -Wl -Map '$@.map' ${CPU} -mthumb -g3 -gdwarf-2"
+    "-T${PROJECT_SOURCE_DIR}/link/XMC${XMC_SERIES}x${XMC_SIZE}.ld -L${XMC_LIB_DIR}/CMSIS/Lib/GCC -nostartfiles ${CPU} -mthumb -g3 -gdwarf-2"
     )
-
-# Processor specific definitions
-add_definitions(-Dgcc)
-
